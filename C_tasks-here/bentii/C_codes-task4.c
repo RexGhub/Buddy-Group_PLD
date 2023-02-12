7641f43870fdc4803c5ed0312b0899e9996a2dc5
Tornyenyo <tornyenyogad@gmail.com>
2023-02-12 21:47:47 +0000

    codes to sum 2 variables

---
+++ C_tasks-here/bentii/C_codes-task4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+int sumOfNumber, firstNumber, secondNumber
+;
+	printf("Enter the first number:");
+
+	scanf("%d", &firstNumber);
+
+	printf("Enter the second number:");
+
+	scanf("%d", &secondNumber);
+
+	sumOfNumber = firstNumber + secondNumber;
+
+	printf("The total of the numbers is: %d",sumOfNumber);
+
+	return 0
+}
